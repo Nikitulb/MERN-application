5d853a0992136a0d9faa97085f778defa5ddc067
Nikita <nickmedvik@gmail.com>
2019-08-01 19:55:01 +0300

    add posts routes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => res.send('Api Running'));
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 
